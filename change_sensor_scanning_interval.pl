fce1b134e445ba33cf8a1174cdad790753cf756a
jordanburch <jordan.burch@richweb.com>
2019-11-22 16:52:25 -0500

    Add files via upload
    updated script to allow multiple sensor types in config at top of script

--- change_sensor_scanning_interval.pl
+++ change_sensor_scanning_interval.pl
@@ -1,13 +1,28 @@
+#! /usr/bin/perl/perl
+
+use lib('/home/jburch/prtg/prtg');
 use strict;
 use PRTG;
 use Data::Dumper qw( Dumper );
 
 
-my $sensors = PRTG::get_sensors_by_type('ping');
 
-foreach my $row(@$sensors){
-    my $id = $row->{'objid'};
-    PRTG::update_interval($id,'60');
+my $config = {
+    'ping' => 60,
+    'wmidiskspace'    => 60*60*2,
+    'wmiprocessor'    => 60*5,
+    'wmimemory'       => 60*5,
+    'wmihypervserver' => 60*5,
+    'wmihyperv'       => 60*5,
+};
+
+foreach my $type (keys(%$config)){
+    my $sensors = PRTG::get_sensors_by_type($type);
+    my $interval = $config->{$type};
+    foreach my $row(@$sensors){
+	my $id = $row->{'objid'};
+	PRTG::update_interval($id,$interval);
+    }
+    
 }
-
 1;
